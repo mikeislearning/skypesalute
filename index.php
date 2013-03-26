6f7833b12ad7e3f00e7f4315457dde86dc3a04e4
mikeislearning <tothemac@gmail.com>
2013-03-25 22:43:36 -0400

    first commit - all files

---
+++ index.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+<!-- Credit to https://wrapbootstrap.com/theme/boot-metro-WB06L327S
+Metro Bootstrap template -->
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>skype SALUTE - The World's First Completely Digital Salutation </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta name="description" content="skype SALUTE - Welcoming the World's First Digital Handshake"> <!-- SEO description go here -->
+   <meta name="keywords" content="skype, salute, skypesalute, salutation, greeting, digital communication, digital gesture, digital hello"> <!-- SEO keywords go here -->
+
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300" rel='stylesheet' type='text/css'>
+
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+ 
+  </head> 
+
+  <body id="home" data-spy="scroll" data-target=".navbar">
+
+  <!-- NAV -->
+  		<nav class="navbar navbar-fixed-top" id="theNav">
+            <section class="navbar-inner" id="nobottom">
+                <article class="container">
+                    <a class="btn btn-navbar m-btn blue" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                    <h1 class="brand">
+                        <a href="#home" class="anchorLink" title="skype salute home">
+                            <img src="img/skype_cloud12.png" alt="skype SALUTE logo" class="mainlogo"/>
+                        </a>                            
+                    </h1>
+                    <aside class="nav-collapse">
+                        <ul class="nav pull-right">
+                          <li><a href="#home" class="anchorLink" title="home">Home</a></li>
+                          <li><a href="#whatitis" class="anchorLink" title="what is it?">Why?</a></li>
+                          <li><a href="#benefits" class="anchorLink" title="skype salute benefits">Benefits</a></li>
+                          <li><a href="#lookpast" class="anchorLink" title="other salutations">Salutations</a></li>
+                          <li><a href="#theform" class="anchorLink" title="send your salute!">Form</a></li>
+                        </ul>
+                    </aside>
+                </article>
+            </section>
+        </nav>
+<!-- END NAV -->
+
+
+<!-- TOP HERO -->
+    
+    <div class="container-fluid cloudback section" id="theTop">
+	<div class="row-fluid">
+    <img src="img/blimp1.png" alt="first parallax blimp" id="parallax-bg1" />
+    <img src="img/blimp2.png" alt="second parallax blimp" id="parallax-bg2" />
+    <img src="img/blimp3.png" alt="third parallax blimp" id="parallax-bg3" />
+    <img src="img/blimp5.png" alt="fifth parallax blimp" id="parallax-bg5" />
+    
+    	<header class="hero-unit" id="main-banner">
+        	
+            <div id="carouselContainer">
+            <div id="myCarousel" class="carousel slide pull-right">
+              <!-- Carousel items -->
+              <div class="carousel-inner">
+                <div class="active item"><img src="img/macbookme.png" alt="put your hand to your forehead" /></div>
+                <div class="item"><img src="img/macbooksalute.png" alt="salute your fellow skyper" /></div>
+                <div class="item"><img src="img/macbookheart.png" alt="press your hand to your heart"/></div>
+              </div>
+              <!-- Carousel nav -->
+              <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+              <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+            </div><!--/myCarousel -->
+            </div><!--/carouselContainer -->
+            <h1> How to do a </h1>
+          
+            <img src="img/skype_cloud12.png" alt="logo" class="headline"/> 
+            <br />
+             
+            <h2 class="headline"> Welcoming the world's first digital salutation</h2>
+            <strong>
+            <a href="#whatitis" class="btn btn-success btn-large anchorLink pushup">...why? </a>
+            </strong>
+            
+        </header >
+        
+    </div><!--/row-fluid -->
+   </div><!--/container-fluid -->
+
+<!-- END TOP HERO -->
+
+
+
+<!-- WHAT IT IS -->
+        <div class="row-fluid fullscreen" id="whatitis">
+          <div class="container section hero-unit disappear pushneg"> 
+          
+              <h2 class="blueh2">A Brief Explanation</h2>
+              <section class="row-fluid span12"> 
+              <article class="span4">
+              <img src="img/handshake.jpg" alt="people shaking hands" class="img-polaroid"/>
+          
+                <h3>The Past</h3>
+                <p>Whether its a handshake, a bow, or a kiss, we have always used gestures as salutations to help settle deals or conflicts.</p>
+                </article>
+              <article class="span4">
+              <img src="img/skypedeal.jpg" alt="people making a deal over skype" class="img-polaroid"  />
+                <h3>The Present</h3>
+                <p>As more people begin to use skype as their main form of communication, they no longer have a such a salutation at their disposal which conveys  this mutual respect.</p>
+              </article>
+              <article class="span4">
+              <img src="img/salutegif.jpg" alt="people performing the skype salute" class="img-polaroid" />
+                <h3>The skype SALUTion</h3>
+                <p class="makeinline"><em>"Hand to the Head for Respect, </em></p>
+                <p><em>Hand to the Heart for Honour." </em></p>
+                <p class="makeinline">The skype SALUTE is the world's first completely digital salutation. </p>
+              </article>
+          	</section> 
+        </div><!-- /container -->
+      </div><!-- /.row-fluid section and #whatitis -->
+       
+<!-- END WHAT IT IS -->    
+        
+
+<!-- BENEFITS -->
+  <div class="container-fluid blueback section fullscreen" id="benefits">
+  	<div class="container-fluid hero-unit">
+      <h2> Benefits</h2>
+    
+      <article class ="row-fluid span12 blueh2 disappear">
+           <div class="span4 offset2">
+          <img src="img/strongshake.jpg" alt="one hand getting squeezed very hard - should've skype saluted" class="img-circle benefitimage" />
+          </div>
+          <div class="span6">
+          	   <h3>No more crushed hands!</h3>
+               <p>Got a weak flimsy handshake? The skype SALUTE gives you all the emotional security of a handshake, with none of the broken metacarpals. </p>
+          </div>
+      </article>
+      <article class ="row-fluid span12 blueh2 disappear">
+          <div class = "span4 offset2">
+   		<img src="img/germaphobe1.jpg" alt="tyrone biggums on drugs, enjoying some crack" class="img-circle benefitimage" />
+          </div>
+          <div class="span6">
+            <h3>No actual touching necessary! </h3>
+          	<p>The skype SALUTE is perfect for germaphobes, and since you don't actually have to leave the house, agoraphobes as well.</p>
+          </div>
+      </article>
+      <article class ="row-fluid span12 blueh2 disappear">
+          <div class="span4 offset2">
+          <img src="img/rockbeat.jpg" alt="handshake or fist bump turns into paper beats rock" class="img-circle benefitimage"/>
+          </div>
+          <div class="span6">
+          	   <h3> Handshake or Fist Bump? </h3>
+              <p> With skype SALUTE, the confusion figuring out which hand gesture to make becomes a thing of the past. Never have your rock be beaten by paper again! </p>
+          </div>
+      </article>
+    </div>
+  </div><!-- /.container #benefits -->
+<!-- END BENEFITS -->
+
+<!-- A LOOK TO THE PAST -->
+        <div class="row-fluid fullscreen" id="lookpast">
+          <div class="container section hero-unit disappear"> 
+              <h2 class="blueh2">Salutations of our Time</h2>
+              <div id="content">
+                <ul id="tabs" class="nav nav-tabs" data-tabs="tabs">
+                    <li class="active"><a href="#traditional" data-toggle="tab">Traditional</a></li>
+                    <li><a href="#national" data-toggle="tab">National</a></li>
+                    <li><a href="#stereotypes" data-toggle="tab">Stereotypes</a></li>
+                    <li><a href="#gangs" data-toggle="tab">Gang Signs</a></li>
+                </ul>
+                <div id="my-tab-content" class="tab-content">
+                    <div class="tab-pane active" id="traditional">
+                        <section class="row-fluid span12"> 
+                          <article class="span4">
+                          <img src="img/handshake.png" alt="hands that are shaking" class="saluteimage" />
+                            <h3>The Handshake</h3>
+                            <p>Common in western cultures.</p>
+                            </article>
+                          <article class="span4">
+                          <img src="img/bow.jpg" alt="people bowing in respect" class="saluteimage" />
+                            <h3>The Bow</h3>
+                            <p class="makeinline">Common in eastern cultures.</p>
+                            <p> And martial arts tournaments.</p>
+                          </article>
+                          <article class="span4">
+                          <img src="img/curtsey.png" alt="girl doing a curtsey" class="saluteimage" />
+                            <h3>The Curtsey</h3>
+                            <p>Common in Buckingham Palace. </p>
+                          </article>
+                        </section> 
+                    </div><!-- /traditional -->
+                    <div class="tab-pane" id="national">
+                          <section class="row-fluid span12"> 
+                          <article class="span4">
+                          <img src="img/sorry.png" alt="canadian saying sorry" class="saluteimage" />
+                            <h3>The Apology</h3>
+                            <p>Often given in Canada.</p>
+                            </article>
+                          <article class="span4">
+                          <img src="img/middlefinger.png" alt="american giving the finger" class="saluteimage" />
+                            <h3>The One Finger Salute</h3>
+                            <p>Often given in the US.</p>
+                          </article>
+                          <article class="span4">
+                          <img src="img/cheekpeck.jpg" alt="a little peck on the cheek" class="saluteimage" />
+                            <h3>The Cheek Peck</h3>
+                            <p>Often given by your attractive cousin that one time, and you really didn't mean to like it so much. </p>
+                          </article>
+                        </section> 
+                    </div><!-- /national -->
+                     <div class="tab-pane" id="stereotypes">
+                          <section class="row-fluid span12"> 
+                          <article class="span4">
+                          <img src="img/head_nod.jpg" alt="how to properly nod your head" class="saluteimage" />
+                            <h3>The Head Nod</h3>
+                            <p> A staple of men everywhere</p>
+                            </article>
+                          <article class="span4">
+                          <img src="img/excitedgirl.png" alt="girl who is very excited" class="saluteimage" />
+                            <h3>The Exclamation</h3>
+                            <p> OH MY GOD, where'd you get your hair done??!?</p>
+                          </article>
+                          <article class="span4">
+                          <img src="img/yolo.png" alt="do you yolo? no, no you don't" class="saluteimage" />
+                            <h3>The YOLO</h3>
+                            <p>No.</p>
+                          </article>
+                        </section> 
+                    </div><!-- /intimate -->
+                     <div class="tab-pane" id="gangs">
+                          <section class="row-fluid span12"> 
+                          <article class="span4">
+                          <img src="img/westside.png" alt="hand making west side symbol" class="saluteimage" />
+                            <h3>West Side</h3>
+                            <p>booyyyyyyyyyyy...</p>
+                            </article>
+                          <article class="span4">
+                          <img src="img/eastside.png" alt="hand making east side symbol" class="saluteimage" />
+                            <h3>East Side</h3>
+                            <p> yyyyyoooooooooo....</p>
+                          </article>
+                          <article class="span4">
+                          <img src="img/gangnam.jpg" alt="gangnam style cartoon" class="saluteimage" />
+                            <h3>Gangnam Style</h3>
+                            <p> Heeeeeeeeeey Sexy Lady </p>
+                          </article>
+                        </section> 
+                    </div><!-- /oy -->
+    </div>
+</div>
+        </div><!-- /container -->
+      </div><!-- /.row-fluid section and #whatitis -->
+<!-- END A LOOK TO THE PAST -->
+
+<!-- THE FORM -->
+<footer class="container-fluid greenback section form" id="theform">
+      	<aside class="row-fluid">      
+        
+            <div class="field span7 offset1">
+             <form> 
+            <h3>Be part of the skype SALUTion!</h3>
+
+            <p>Send your pictures in to have them posted at the top of the website</p>             
+                        <div class="field span8">
+                          <input type="text" value="" class="input-text m-wrap m-ctrl-large" id="name" name="name" placeholder="Your Name"/>
+                          </div>
+                        <div class="field span8">
+                          <input type="text" value="" class="input-text m-wrap m-ctrl-large" id="email" name="email"  placeholder="Your Email Address" />
+                        </div>
+                        <div class="field span8">
+                        <input type="file" value="Upload your photo" onchange="handleFiles(this.files)"/>
+                        </div>
+                        <div class="field span8">
+                        <input type="submit" class="btn btn-large btn-success" value="Submit" />
+                     	 </div>
+                   
+                    </form>
+          	
+          </div>
+          <div class="span4">
+      		<h1>Website by: </h1>
+            <p><a href="http://www.mikeislearning.com" alt="My personal website">Michael MacPherson </a> - Design and Development</p>
+            <p><a href="#" alt="josh's back will one day go here">Joshua Gillis</a>  - Photography </p>
+            <p>Thanks to <a href="http://twitter.github.com/bootstrap/" alt="twitter bootstrap" > Twitter Bootstrap </a>for existing </p>                  
+           </div>  
+         </aside>
+</footer>
+
+<!-- END THE FORM -->
+
+  </body>
+    <!-- Placed at the end of the document so the pages load faster -->
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap-scrollspy.js"></script>
+  <script src="js/anchor.js"></script>
+  <script src="js/bootstrap.js"></script>
+  
+  <script>
+  //this whole thing is for parallax scrolling
+  $(window).bind('scroll',function(e){
+    parallaxScroll();
+});
+ 
+function parallaxScroll(){
+    var scrolled = $(window).scrollTop();
+    $('#parallax-bg1').css('top',(0-(scrolled*.80))+'px');
+    $('#parallax-bg2').css('top',(0-(scrolled*.50))+'px');
+    $('#parallax-bg3').css('top',(0-(scrolled*.28))+'px');
+	$('#parallax-bg4').css('top',(0-(scrolled*.85))+'px');
+	$('#parallax-bg5').css('top',(0-(scrolled*.60))+'px');
+}
+
+
+</script>
+<script type="text/javascript">
+//this whole thing is for tabs
+    jQuery(document).ready(function ($) {
+		//creates tabs for the benefits
+        $('#tabs').tab();
+		/*hopefully resizes the window to take up the fullscreen for the following divs..but it screws up responsivity
+		$(window).resize(function() {
+			$('#theTop').height($(window).height());
+			$('#theTop').width($(window).width());
+			$('#whatitis').height($(window).height());
+			$('#whatitis').width($(window).width());
+			$('#benefits').height($(window).height());
+			$('#benefits').width($(window).width());
+			$('#lookpast').height($(window).height());
+			$('#lookpast').width($(window).width());
+		});
+		*/
+		
+    });
+</script> 
+</html>
